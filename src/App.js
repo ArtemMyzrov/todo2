027e1307f39bec2540c7efc7e90aae1f4a8c6edc
artemmyzrov <a.myzrov@yandex.ru>
2023-02-20 17:54:39 +0400

    feat: add application structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
+import List from './components/List'
 
 function App() {
+  const [tasks, setTask] = useState([])
+  const [text, setText] = useState('')
+
+  const addText = () => {
+    addTask(text)
+    setText('')
+  }
+
+  const addTask = (text) => {
+    if (text.length === 0) {
+      return
+    }
+    if (tasks.filter((e) => e.title === text).length > 0) {
+      return
+    }
+    setTask([...tasks, { title: text, isDone: false }])
+  }
+
+  console.log(tasks)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="App">
+        <div id="myDIV" className="header">
+          <h2>Task List</h2>
+          <div className="taskForm">
+            <input
+              onChange={(e) => setText(e.target.value)}
+              value={text}
+              type="text"
+              id="myInput"
+              placeholder="Enter a new task"
+            />
+            <span onClick={addText} className="addBtn">
+              Add
+            </span>
+          </div>
+        </div>
+        <List tasks={tasks} />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
